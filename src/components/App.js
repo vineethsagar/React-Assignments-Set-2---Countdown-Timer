6f0feea3b51f32e533046356efbdf75cf80a1409
vineethsagar <30525698+vineethsagar@users.noreply.github.com>
2020-12-07 22:40:37 +0530

    done using interval attempt - 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,47 @@
 import React, { Component, useState, useEffect } from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 
 const App = () => {
-  // write your code here 
+  // write your code here
+  const [time, setTime] = useState(0);
+  const handleKeyDown = (event) => {
+    if (event.keyCode !== 13) {
+      console.log("Not enter");
+      return;
+    }
+
+    if (isNaN(event.target.value)) {
+      setTime(0);
+      return;
+    }
+    let inputTime = Math.floor(event.target.value);
+    setTime(inputTime);
+  };
+
+  const decrement = () => {
+    setTime((time) => time - 1);
+  };
+  useEffect(() => {
+    if (time === 0) {
+      return;
+    }
+    const id = setInterval(decrement, 1000);
+    return () => {
+      clearInterval(id);
+    };
+  }, [time]);
 
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for
+          <input id="timeCount" onKeyDown={handleKeyDown} /> sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{time}</div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
